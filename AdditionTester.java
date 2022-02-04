35535d1bb14e5ebedc840df6c5d2a8f6d5c09ac1
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2022-02-03 21:30:50 -0800

    fix

--- AdditionTester.java
+++ AdditionTester.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public AdditionTester{
+public class AdditionTester{
 	@Test
 	public void testAdd(){
 		assertEquals("1 + 1 = 3, will fail", Addition.add(1,1), 3);
