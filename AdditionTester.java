18d08627004d2bc26de523811e1aa289ac9b52c8
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2022-02-03 21:32:35 -0800

    more tests

--- AdditionTester.java
+++ AdditionTester.java
@@ -4,6 +4,9 @@ import org.junit.*;
 public class AdditionTester{
 	@Test
 	public void testAdd(){
-		assertEquals("1 + 1 = 3, will fail", Addition.add(1,1), 3);
+		//assertEquals("1 + 1 = 3, will fail", Addition.add(1,1), 3);
+		assertEquals("1 + 1 = 2", Addition.add(1,1), 2);
+		assertEquals("10 + 10 = 20", Addition.add(10, 10), 20);
+		assertEquals("-1 + 1 = 0", Addition.add(-1,1), 0);
 	}
 }
